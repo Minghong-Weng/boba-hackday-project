cdd5b0c732f5b51be21fa0408ffdfd2e11e0377c
Yuan Fang <yuan.fang@mycase.com>
2021-10-28 19:14:34 -0700

    get real data

--- boba-hackday-project/public/dataManager.js
+++ boba-hackday-project/public/dataManager.js
@@ -1,28 +1,38 @@
 class DataManager {
   static getRestaurant(id) {
+    const fs = require('fs')
+
+    var review
+    fs.readFile('../../data/data.json', 'utf8', (err, jsonString) => {
+      if (err) {
+        console.log("Error reading file from disk:", err)
+        return
+      }
+      try {
+        review = jsonString
+        console.log("Yelp Review is", review)
+      } catch (err) {
+        console.log('Error parsing JSON string:', err)
+      }
+    })
+
+    var parsed_review = JSON.parse(review.replace(/\n/g, ''))
+
+    const map = new Map();
+    for (var k of Object.keys(parsed_review)) {
+      map.set(k, parsed_review[k]);
+    }
+
+    const restaurant_data = map.get(String(id));
     const data = {
-      name: 'tan cha',
-      yelp_url: 'url',
-      yelp_rating: 5,
-      shop_restaurant_mc_score: 5,
-      reviews: [
-        {
-          text: 'great place',
-          yelp_rating: 4,
-          mycase_score: 4,
-          id: 1,
-        },
-        {
-          text: 'not a great place',
-          yelp_rating: 1,
-          mycase_score: 1,
-          id:2,
-        }
-      ],
+      name: restaurant_data.name,
+      yelp_url: restaurant_data.yelp_url,
+      yelp_rating: restaurant_data.yelp_rating,
+      shop_restaurant_mc_score: restaurant_data.shop_restaurant_mc_score,
+      reviews: restaurant_data.reviews,
     }
     return data;
   }
-
 }
 
 export default DataManager;
