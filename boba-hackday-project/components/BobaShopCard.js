c10338a293efb829ed844c158445e48f0cd5c6a3
vtyeh <vtyeh24@gmail.com>
2021-10-28 17:02:52 -0700

    create BobaShopCard component, styling

---
+++ boba-hackday-project/components/BobaShopCard.js
@@ -0,0 +1,30 @@
+import styles from '../styles/Home.module.css'
+import { Rating } from '@mui/material';
+
+export default function BobaShopCard({ bobaShop }) {
+ return (
+     <>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>{ bobaShop.name }</h2>
+            <p className={styles.rating}>
+                <span>MyCase Review: </span>
+                <Rating 
+                    name="mycase-rating" 
+                    defaultValue={bobaShop.shop_restaurant_mc_score} 
+                    precision={0.5} 
+                    readOnly 
+            /></p>
+            <p className={styles.rating}>
+                <span>Yelp Review: </span>
+                <Rating 
+                    name="yelp-rating" 
+                    defaultValue={bobaShop.yelp_rating} 
+                    precision={0.5} 
+                    readOnly 
+                />
+            </p>
+            <a href={ bobaShop.yelp_url } className={styles.yelpURL}>Visit on Yelp</a>
+        </a>
+     </>
+ )
+}
